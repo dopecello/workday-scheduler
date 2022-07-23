9873061d11c2f0b50e1a2d5630720b29bfe87e5c
dopecello <williamlocke.cello@gmail.com>
2022-07-23 17:35:11 -0400

    save and load from localStorage

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,8 @@ function timeDisplay() {
     let rightNow = moment().format('MMMM Do YYYY, h:mm:ss A');
     timeSelector.innerHTML = rightNow
 
-}
-timeDisplay()
+};
+
 
 function auditTimes() {
     let currentHour = moment().hour();
@@ -27,7 +27,31 @@ function auditTimes() {
             $(this).addClass("future")
         }
     })
-}
+};
+
+
+function saveSchedule() {
+  $(".saveBtn").on("click", function() {
+    time = $(this).parent($(".row time-block")).attr("id")
+    text = $(this).siblings(".description").val()
+    localStorage.setItem(time, text)
+  })
+};
+
+
+function loadSchedule() {
+$("#9text").val(localStorage.getItem("9"))
+$("#10text").val(localStorage.getItem("10"))
+$("#11text").val(localStorage.getItem("11"))
+$("#12text").val(localStorage.getItem("12"))
+$("#13text").val(localStorage.getItem("13"))
+$("#14text").val(localStorage.getItem("14"))
+$("#15text").val(localStorage.getItem("15"))
+$("#16text").val(localStorage.getItem("16"))
+$("#17text").val(localStorage.getItem("17"))
+};
+
+timeDisplay()
 auditTimes()
-
-
+saveSchedule();
+loadSchedule();
