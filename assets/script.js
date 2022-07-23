a2872105c6ac4d7f2a59417e173d6efcc202c941
dopecello <williamlocke.cello@gmail.com>
2022-07-23 16:51:26 -0400

    auditing time function

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,33 @@
+
 //Setting Time Display
 const timeSelector = document.getElementById("currentDay")
 let rightNow = moment().format('MMMM Do YYYY, h:mm:ss A');
-function timeDisplay () {
-timeSelector.innerHTML = rightNow
+function timeDisplay() {
+    timeSelector.innerHTML = rightNow
 }
 timeDisplay()
 //End Time Display
+
+function convertTimes() {
+    let currentHour = moment().hour();
+    $(".time-block").each(function () {
+        const timeBlock = parseInt($(this).attr("id"))
+        if (currentHour > timeBlock) {
+            $(this).addClass("past")
+            $(this).removeClass("present")
+            $(this).removeClass("future")
+        }
+        else if (currentHour === timeBlock) {
+            $(this).removeClass("past")
+            $(this).addClass("present")
+            $(this).removeClass("future")
+        }
+        else {
+            $(this).removeClass("past")
+            $(this).removeClass("present")
+            $(this).addClass("future")
+        }
+    })
+}
+convertTimes()
 
